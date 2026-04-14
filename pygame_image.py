312c579d548cbf3652dc627c475307becd9c1906
hara <c0a2521034@edu.teu.ac.jp>
2026-04-14 15:40:38 +0900

    練習４と練習４

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    k3_image =pg.image.load("fig/3.png")#工科トン画像
+    k3_image=pg.transform.flip(k3_image, True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #練習2
+        screen.blit(k3_image, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
